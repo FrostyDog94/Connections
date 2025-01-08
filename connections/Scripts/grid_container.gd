78af0881e8731e30b0ea1f780def1418c8181cf4
Andrew Soguero <andrewsoguero@gmail.com>
2025-01-08 00:03:06 -0700

    UI

--- connections/Scripts/grid_container.gd
+++ connections/Scripts/grid_container.gd
@@ -1,13 +1,15 @@
 extends GridContainer
-var x = 0
-var y = 0
+var x = randi_range(0, 3)
+var y = randi_range(0, 3)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for n in self.get_children():
-		get_child(x).text = Global.list[y][y]
-		x += 1
-
+		n.text = Global.list[x][y]
+		
+		x = randi_range(0, 3)
+		y = randi_range(0, 3)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
